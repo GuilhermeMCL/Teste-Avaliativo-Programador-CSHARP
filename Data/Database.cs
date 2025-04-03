6c6ecca09aa7b7d7e2ccca31d3bf00cc0d43a47d
Guilherme Maciel <guilhermemcl1503@gmail.com>
2025-04-03 06:44:10 -0300

    feat : iniciando projeto e conexão com banco de dados

---
+++ Data/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace cadastroFornecedores.Data
+{
+   public sealed class Database 
+   {
+        private static Database _instance;
+        private MySqlConnection _connection;
+
+        private string _connectionString = "Server=localhost;Database=fornecedores_db;User=root;Password=root;";
+
+        private Database()
+        {
+            _connection = new MySqlConnection(_connectionString);
+        }
+
+        public static Database GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Database();
+            }
+            return _instance;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+                _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+                _connection.Close();
+        }
+
+    }
+}
